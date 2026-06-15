9fd9c4fe8a9a7278a6ac2fbeaef39131672fa641
crix <mjun@kakao.com>
2026-06-15 21:29:30 +0900

    Chromium을 default webbrowser로

--- home/crix/common.nix
+++ home/crix/common.nix
@@ -31,4 +31,14 @@
   };
 
   programs.chromium.enable = true;
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "chromium-browser.desktop";
+      "x-scheme-handler/http" = "chromium-browser.desktop";
+      "x-scheme-handler/https" = "chromium-browser.desktop";
+      "x-scheme-handler/about" = "chromium-browser.desktop";
+      "x-scheme-handler/unknown" = "chromium-browser.desktop";
+    };
+  };  
 }
