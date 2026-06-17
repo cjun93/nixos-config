d0ef8f8f6fd5064f0ea7557749a0ecedcba136f5
crix <mjun@kakao.com>
2026-06-17 15:25:40 +0900

    Add usbutils, tidy systemPackages comments

--- modules/base.nix
+++ modules/base.nix
@@ -9,12 +9,9 @@
   environment.variables.EDITOR = "emacs -nw";
 
   environment.systemPackages = with pkgs; [
-    # 기본 유틸리티
     wget git tree
-
-    # 압축/해제 (CLI). Thunar archive plugin 의 백엔드 포맷 처리도 겸함
     zip unzip p7zip gzip bzip2 xz zstd
-    unrar              # rar 해제 (unfree). 불필요 시 제거
+    unrar usbutils
   ];
 
   programs.nano.enable = false;
